b8d8035fe06a2c85fb8a6b952158e0c981b5f227
Grisha <Grisha@DESKTOP-QDC8PJ1>
2024-11-07 17:24:53 +0300

    Добавьте файлы проекта.

---
+++ лчдсоядлчсо/Program.cs
@@ -0,0 +1,45 @@
+﻿//1 ФИО
+//using Microsoft.VisualBasic;
+
+//Console.WriteLine("Введите свою фамилию");
+
+//var Famali = Console.ReadLine();
+//Console.WriteLine("введите своё имя");
+//var name = Console.ReadLine();
+//Console.WriteLine("Ввидите свое отчество");
+//var patronymic = Console.ReadLine();
+//var FNP = "" + Famali + " " + name + " " + patronymic;
+
+//Console.WriteLine(FNP);
+//2 дата рождения
+//using System;
+//String data_year = Console.ReadLine();
+//int number_data_year = System.Convert.ToInt32(data_year);
+//String data_m = Console.ReadLine();
+//int number_data_m = System.Convert.ToInt32(data_m);
+//int data_you = 2024 - number_data_year;
+//Console.WriteLine(data_you);
+//3 номер телфона
+//using System;
+//using System.Collections;
+//Console.WriteLine("Введите свой номер телефона");
+//String phone = Console.ReadLine();
+//if (phone[0] == '7')
+//    {
+//    phone = "+" + phone; 
+//    }
+//else if (phone[0] == '8')
+//    {
+//    phone =phone.Substring(1);
+//    phone ="+"+ "7" + phone; 
+//     }
+//else 
+//    {
+//    Console.WriteLine("номер введен не коректно");
+//     }`1
+//Console.WriteLine("Ваш номер телефона:"+phone);
+//4 почтаe
+//Console.WriteLine("Введите свою почту");
+//String maill = Console.ReadLine();
+//Console.WriteLine("Ваша почта:"maill);
+
